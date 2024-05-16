cdce008c03037cdc9c3415bd612a65ddec666ea3
Pajtus <piotrus.umanski@gmail.com>
2024-05-16 19:38:32 +0200

    Podstawowa implementacja klasy Pionek

--- src/Pionek.py
+++ src/Pionek.py
@@ -1,9 +1,20 @@
-# class Pionek:
-#     def __init__(self, numerPola: int, color: Color, grafika: Graphic):
-#         self.numerPola = numerPola
-#         self.color = color
-#         self.grafika = grafika
-#
-#     def zmienGrafike(self) -> bool:
-#
-#     def zmienPozycje(self, nowaPozycja: int) -> bool:
+import pygame
+
+class Pionek:
+    MIN_LICZBA_OCZEK = 2
+    MAX_LICZBA_OCZEK = 12
+
+    def __init__(self, numer_pola: int, color: pygame.color.THECOLORS, grafika: str):
+        self.numer_pola = numer_pola
+        self.color = color
+        self.grafika = grafika
+
+    def zmien_grafike(self) -> bool:
+        pass
+
+    def przesun(self, liczba_pol: int) -> bool:
+        if Pionek.MIN_LICZBA_OCZEK <= liczba_pol <= Pionek.MAX_LICZBA_OCZEK:
+            self.numer_pola += liczba_pol
+            return True
+
+        return False
