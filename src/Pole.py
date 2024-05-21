671e9ad02a9747afc233acaf722a3bd48768f4da
Pajtus <piotrus.umanski@gmail.com>
2024-05-21 22:39:45 +0200

    nowe renderowanie nieskonczone

--- src/Pole.py
+++ src/Pole.py
@@ -9,11 +9,12 @@ class Vector2(NamedTuple):
 
 
 class Pole:
-    WYMIAR_NAGLOWKA: Vector2 = (30, 50)
-    MALE_POLE_WYMIARY: Vector2 = (30, 50)
-    DUZE_POLE_WYMIARY: Vector2 = (50, 50)
-    KOLOR_TLA = pygame.color.THECOLORS["white"]
-    OFF_SET: Vector2 = (100, 100)
+    WYMIAR_NAGLOWKA: Vector2 = Vector2(30, 50)
+    MALE_POLE_WYMIARY: Vector2 = Vector2(30, 50)
+    DUZE_POLE_WYMIARY: Vector2 = Vector2(50, 50)
+    KOLOR_TLA = pygame.color.THECOLORS["red"]
+    OFF_SET: Vector2 = Vector2(100, 100)
+    SPACING: int = 10
 
     """
     numer_pola - nr. id pola [0, maksymalna_liczba_pol)
@@ -25,7 +26,7 @@ class Pole:
         if abs(maksymalna_liczba_pol / dlugosc_sciany_w_polach) != len(Kierunek):
             raise "Podano niepoprawne wymiary planszy!"
 
-        return Kierunek((numer_pola % maksymalna_liczba_pol) // (dlugosc_sciany_w_polach))
+        return Kierunek((numer_pola % maksymalna_liczba_pol) // dlugosc_sciany_w_polach)
 
     @staticmethod
     def oblicz_rozmiar_pola(numer_pola, dlugosc_sciany_w_polach, maksymalna_liczba_pol) -> Vector2:
@@ -46,22 +47,30 @@ class Pole:
         kierunek_sciany = self.oblicz_zwrot_naglowka_pola(self.numer, 10, 40)
         wymiary_pola = self.oblicz_rozmiar_pola(self.numer, 10, 40)
 
-        left = Pole.OFF_SET.x
-        top = Pole.OFF_SET.y
+        lewo = Pole.OFF_SET.x
+        gora = Pole.OFF_SET.y
 
+        szerokosc_aktualny_kierunek = wymiary_pola.x if kierunek_sciany in (Kierunek.Gora, Kierunek.Dol) else wymiary_pola.y
+        wysokosc_aktualny_kierunek = wymiary_pola.y if kierunek_sciany in (Kierunek.Gora, Kierunek.Dol) else wymiary_pola.x
+
+        # szerokosc_przeciwny_kierunek = wymiary_pola.y if kierunek_sciany in (Kierunek.Gora, Kierunek.Dol) else wymiary_pola.x
+        # wysokosc_przeciwny_kierunek = wymiary_pola.x if kierunek_sciany in (Kierunek.Gora, Kierunek.Dol) else wymiary_pola.y
+
+        # Uwaga na orientacje dlugosci i szerokosci pola, jako x i y, zawsze os pozioma to x, os pionowa to y, niezaleznie od orientacji pola, nieintuicyjne!
         match kierunek_sciany:
             case Kierunek.Gora:
-                left += (self.numer % 10) * wymiary_pola.x
+                lewo += ((self.numer % 10) * (Pole.MALE_POLE_WYMIARY.x + Pole.SPACING)
+                         + (Pole.DUZE_POLE_WYMIARY.x if self.numer % 10 != 0 else 0))
 
             case Kierunek.Prawo:
-                left += 10 * wymiary_pola.y
-                top += (self.numer % 10) * wymiary_pola.y
+                lewo += 9 * (Pole.MALE_POLE_WYMIARY.x + Pole.SPACING) + Pole.DUZE_POLE_WYMIARY.x
+                gora += (self.numer % 10) * (Pole.MALE_POLE_WYMIARY.x + Pole.SPACING)
 
             case Kierunek.Dol:
-                left += 10 - (self.numer % 10) * wymiary_pola.x
+                lewo += (10 - (self.numer % 10)) * (Pole.MALE_POLE_WYMIARY.x + Pole.SPACING) + (Pole.DUZE_POLE_WYMIARY.x if self.numer % 10 == 0 else 0)
+                gora += 9 * (Pole.MALE_POLE_WYMIARY.x + Pole.SPACING) + Pole.DUZE_POLE_WYMIARY.y
 
             case Kierunek.Lewo:
-                top += 10 - (self.numer % 10) * wymiary_pola.y
+                gora += (10 - (self.numer % 10)) * (Pole.MALE_POLE_WYMIARY.x + Pole.SPACING)
 
-
-        pygame.draw.rect(screen, Pole.KOLOR_TLA, pygame.Rect())
+        pygame.draw.rect(screen, Pole.KOLOR_TLA, pygame.Rect(lewo, gora, szerokosc_aktualny_kierunek, wysokosc_aktualny_kierunek), width=1)
