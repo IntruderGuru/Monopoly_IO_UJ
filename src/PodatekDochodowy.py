1b02f48854c975a377baf03e50b6a2185b286326
jrabbitk <kuba.krolikowski@icloud.com>
2024-05-25 12:28:39 +0200

    dodanie do pola podatku dochodowego metody obslugującej płacenie podatku

--- src/PodatekDochodowy.py
+++ src/PodatekDochodowy.py
@@ -13,3 +13,17 @@ class PodatekDochodowy(Pole):
 
     def wyswietl_info(self):
         return (f"Stanąłeś na polu podatek dochodowy. Musisz zapłacić podatek w wysokości {self.podatek}")
+
+    def zaplac_podatek(self, gra: Gra, gracz: Gracz):
+        #cos zagadki wybor i jej zadanie
+        czy_dobra = True
+        if(czy_dobra):
+            return
+        if(self.podatek > gracz.kwota):
+            if gra.pobierz_info_tak_nie("Nie masz wystarczająco dużo pieniędzy, aby zapłacić podatek. Czy chcesz zastawić którąś z nieruchmości? Jeśli tego nie zrobisz przegrywasz."):
+                gracz.zastaw_nieruchomosci()
+            else: 
+                gra.messages.append("Bankrutujesz")
+                return
+        gracz.kwota -= self.podatek
+        gra.messages.append("Podatek został zapłacony")
