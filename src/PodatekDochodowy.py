148118ffca53acb7b1e5c7d1d4a78735dde707b0
jrabbitk <kuba.krolikowski@icloud.com>
2024-05-25 16:38:24 +0200

    dodanie mechanizmu dostawania kolejnych zagadek

--- src/PodatekDochodowy.py
+++ src/PodatekDochodowy.py
@@ -1,6 +1,9 @@
 from src.Pole import Pole
 from src.Gracz import Gracz
 from src.Gra import Gra
+from numpy import random
+from enum import Enum
+
 
 class Odpowiedz(Enum):
     Odpowiedz_A = 0
@@ -8,36 +11,46 @@ class Odpowiedz(Enum):
     Odpowiedz_C = 2
 
 class Zagadka():
-    def __init__(self, tresc_zagadki: str, odpowiedz_a: str, odpowiedz_b: str, odpowiedz_c: str, poprawna: Odpowiedz):
+    def __init__(self, tresc_zagadki: str, odpowiedz_a: str, odpowiedz_b: str, odpowiedz_c: str, poprawna: Odpowiedz) -> None:
         self.tresc_zagadki = tresc_zagadki
         self.odpowiedz_a = odpowiedz_a
         self.odpowiedz_b = odpowiedz_b
         self.odpowiedz_c = odpowiedz_c
         self.poprawna = poprawna
     
-    def zadaj_zagadke(self, gra: Gra) -> bool:
-        gra.messages.append(f"{self.tresc_zagadki} \na. {self.odpowiedz_a} \nb. {self.odpowiedz_b} \nc. {self.odpowiedz_c}")
-        #dodac przyciski na wybieranie odpowiedzi
+class Zagadki:
+    lista_zagadek = [
+        Zagadka()
+    ]
+
+    def __init__(self) -> None:
+        self.permutacja = random.permutation(self.lista_zagadek)
+        self.ind = 0
+
+    def nastepna_zagadka(self) -> Zagadka:
+        curr = self.ind
+        self.ind = (self.ind + 1) % len(self.lista_zagadek)
+        return self.permutacja[curr]
 
 class PodatekDochodowy(Pole):
 
-    def __init__(self, numer: int, podatek: int):
+    def __init__(self, numer: int, podatek: int) -> None:
         super().__init__(numer, "Podatek Dochodowy")
         self.podatek = podatek
 
-    def wyswietl_info(self):
+    def wyswietl_info(self) -> str:
         return (f"Stanąłeś na polu podatek dochodowy. Musisz zapłacić podatek w wysokości {self.podatek}")
 
-    def zaplac_podatek(self, gra: Gra, gracz: Gracz):
-        #cos zagadki wybor i jej zadanie
-        czy_dobra = True
+    def zaplac_podatek(self, gra: Gra, gracz: Gracz, czy_dobra: bool) -> None:
+        do_zaplaty = self.podatek
         if(czy_dobra):
-            return
-        if(self.podatek > gracz.kwota):
+            do_zaplaty /= 2
+        
+        if(do_zaplaty > gracz.kwota):
             if gra.pobierz_info_tak_nie("Nie masz wystarczająco dużo pieniędzy, aby zapłacić podatek. Czy chcesz zastawić którąś z nieruchmości? Jeśli tego nie zrobisz przegrywasz."):
                 gracz.zastaw_nieruchomosci()
             else: 
                 gra.messages.append("Bankrutujesz")
                 return
-        gracz.kwota -= self.podatek
+        gracz.kwota -= do_zaplaty
         gra.messages.append("Podatek został zapłacony")
