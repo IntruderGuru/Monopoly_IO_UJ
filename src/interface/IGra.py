8dcae8be11fa38744c714af29e7089a27f0054ca
Pajtus <piotrus.umanski@gmail.com>
2024-05-30 03:34:07 +0200

    Nowe abstrakcyjne metody

--- src/interface/IGra.py
+++ src/interface/IGra.py
@@ -6,3 +6,11 @@ class IGra:
     @abstractmethod
     def aktualizacja_zdarzenia(self, event: pygame.event.Event):
         pass
+
+    @abstractmethod
+    def aktualizacja(self):
+        pass
+
+    @abstractmethod
+    def wyswietl(self):
+        pass
